6fd5da882af6a182bd98a2d40e9c4beb5bc65029
DSayeins <djibrilla414@gmail.com>
2022-03-14 02:26:01 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*
+	 * Description: Complete the source code in order to print
+	 * the last digit of the number stored in the variable n.
+	 */
+	int i;
+
+	/* your code goes there */
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
