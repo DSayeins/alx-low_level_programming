5cdcb89747a8aa93f536edcfc933739a68f7e2d8
DSayeins <djibrilla414@gmail.com>
2022-03-14 01:21:06 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*
+	 * Description: Complete the source code in order to print
+	 * the last digit of the number stored in the variable n.
+	 */
+	int i;
+
+	/* your code goes there */
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (i = 64; i <= 90; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
