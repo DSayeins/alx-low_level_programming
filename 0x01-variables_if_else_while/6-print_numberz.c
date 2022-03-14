959992281df319316950ab2da8b48a63874abc7e
DSayeins <djibrilla414@gmail.com>
2022-03-14 01:56:33 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*
+	 * Description: Complete the source code in order to print
+	 * the last digit of the number stored in the variable n.
+	 */
+	int i;
+
+	/* your code goes there */
+	for (i = 48; i < 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
