c3985813396a11d38f260770f79df843ca50283a
DSayeins <djibrilla414@gmail.com>
2022-03-16 06:38:22 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,12 @@ void print_alphabet_x10(void)
 	int i = 0;
 	int j = 0;
 
-	while(j < 10)
+	while(j = 0; j < 10; j++)
 	{
 		for (i = 97; i <= 122; i++)
 		{
 			_putchar(i);
 		}
 		_putchar('\n');
-
-		j++;
 	}
 }
