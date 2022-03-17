9871b8ec1fb1de0dbf8103a85c4050fbba3514c9
DSayeins <djibrilla414@gmail.com>
2022-03-17 05:02:00 -0700

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * * _isupper - Entry point
+ * * _isdigit - Entry point
  * * @c: the character to print
  * * Return: 0 or 1.
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 	{
