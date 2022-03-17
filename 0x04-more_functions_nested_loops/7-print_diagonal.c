f631151f27702e929be3dd920614f30b57602315
DSayeins <djibrilla414@gmail.com>
2022-03-17 06:29:20 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * * print_diagonal - Entry point
+ * * @n: integer
+ * * Return: n
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 0; j < i; i++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
