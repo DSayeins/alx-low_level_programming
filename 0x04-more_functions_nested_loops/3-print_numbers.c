ac62709421e4cd0f39f2201efa57248328eeb48a
DSayeins <djibrilla414@gmail.com>
2022-03-17 05:18:55 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * * print_numbers - Entry point
+ * * Return: 0 or 1.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
