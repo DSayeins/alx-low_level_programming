6a4243f4f09420f7b54036c12eb49943a1e0cdf7
DSayeins <djibrilla414@gmail.com>
2022-03-17 06:02:45 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,28 @@
 #include "main.h"
 
 /**
- * * more_number - Entry point
+ * * more_numbers - Entry point
  */
 
 void more_numbers(void)
 {
 	int i;
 	int j;
+	int un;
+	int ten;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 14; j++)
 		{
+			un = i % 10;
+			ten = i / 10;
+
 			if (j > 9)
 			{
-				_putchar((i / 10) + '0');
+				_putchar(ten + '0');
 			}
-			_putchar((i % 10)) + '0');
+			_putchar(un + '0');
 		}
 		_putchar('\n');
 	}
